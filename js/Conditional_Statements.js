67e42c4b5d72b1238573d5a0217cde382380d206
mdibrahim060 <mohdibrahimpov@gmail.com>
2026-07-13 20:20:47 +0530

    Update Conditional_Statements.js

--- js/Conditional_Statements.js
+++ js/Conditional_Statements.js
@@ -1,1 +1,12 @@
 let a = prompt("Hey Whats your age?");
+a = Number.parseInt(a);                       // convert the string to a number.
+// console.log(typeof a)                     // it tels about type of a.
+
+if(a>0){
+    alert("This is a Valid age");
+}
+// it work when condition is true , if condition is fals it doesn't work.
+else{
+    alert("This is an invalid age");
+}
+// if condition is false then else block is run.
